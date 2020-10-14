330cd7e4157da172ee0d515edd57faf23868e580
Saravanan KR <skramaja@redhat.com>
2020-10-14 20:09:24 +0530

    Add ocp config file for 4.5

---
+++ ocp/4.5/config_kni.sh
@@ -0,0 +1,16 @@
+# brq cluster
+export OPENSHIFT_RELEASE_TYPE=ga
+export OPENSHIFT_VERSION=4.5.0
+export PERSONAL_PULL_SECRET=/home/kni/pull-secret.json
+export IP_STACK=v4
+export NUM_WORKERS=0
+export PRO_IF=eno2
+export INT_IF=eno3
+export CLUSTER_NAME=vnf
+export BASE_DOMAIN=skramaja.metal3.io
+export PROVISIONING_NETWORK=172.51.0.0/16
+export MASTER_MEMORY=16384
+export MASTER_DISK=40
+export MASTER_VCPU=8
+export EXTERNAL_SUBNET_V4=192.168.212.0/24
+export WORKING_DIR=/home/dev-scripts
